9c5b5e2adaf5dcd7aa9d29fb013f80b87076b3ac
James Marino <james@marino.io>
2019-05-30 01:18:47 +1000

    Layout Updates

--- app/src/constants/routes.constants.js
+++
@@ -1,7 +0,0 @@
-const staticRoutes = {
-    home: '/',
-    newDeployment: '/deployments/new',
-    listDeployments: '/deployments/',
-};
-
-export { staticRoutes };
--- app/src/styles/css/components/DeploymentForm.css.js
+++
@@ -1,7 +0,0 @@
-const styles = theme => ({
-    container: {
-        padding: theme.defaultPadding,
-    },
-});
-
-export default styles;
